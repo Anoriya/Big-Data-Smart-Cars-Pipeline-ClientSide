1c1f0a3984f8d7de547919b7a3f831562e8565e7
rached1997 <bouchoucharached@gmail.com>
2020-05-11 16:33:03 +0100

    Empatica added

---
+++ Empatica/empatica_producer_BVP.py
@@ -0,0 +1,21 @@
+import csv
+from time import sleep
+#from kafka import KafkaProducer
+from kafka import KafkaProducer
+
+
+class EmpaticaProducerBVP:
+
+    def __init__(self):
+        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+    def start_streaming(self):
+        with open("/home/rached/Bureau/data/5-mcluet/EMPATICA/BVP.csv",
+                  newline='') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                string = ';'.join(row) + "\n"
+                self.producer.send('test', value=string.encode())
+                print("sent")
+                # sleep(1)
