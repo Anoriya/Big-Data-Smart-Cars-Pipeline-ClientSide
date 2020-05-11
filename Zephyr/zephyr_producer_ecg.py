4ab1d748150e600b202f3a88a75ab9b2a9fb2f31
Anoriya <bouhaa7@gmail.com>
2020-05-11 16:20:32 +0100

    Added producers

--- Zephyr/zephyr_producer_ecg.py
+++ Zephyr/zephyr_producer_ecg.py
@@ -2,18 +2,18 @@ import csv
 from kafka import KafkaProducer
 
 
-class CameraProducer:
+class ZephyProducerECG:
 
     def __init__(self):
         self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
     def start_streaming(self):
-        with open("/home/sartharion/Bureau/5-mcluet/CAMERA/compressedAVI_v6.7.5_2_7-13_24_32-1280_800-30.avi.csv",
+        with open("/home/sartharion/Bureau/5-mcluet/ZEPHYR/2020_02_07__13_18_12_ECG.csv",
                   newline='') as f:
             reader = csv.reader(f)
             next(reader)
             for row in reader:
                 string = ';'.join(row) + "\n"
-                self.producer.send('test', value=string.encode())
+                self.producer.send('Zephyr', value=string.encode(), key="ECG".encode())
                 print("sent")
                 # sleep(1)
